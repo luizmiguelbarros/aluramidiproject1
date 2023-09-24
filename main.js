fcc2f4ec19aed56e13c627b5d660ef69b757e058
Luiz Miguel <125470344+luizmiguelbarros@users.noreply.github.com>
2023-09-23 21:43:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0;
+
+// enquanto
+
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+   // template string
+
+    const idAudio = `#som_${instrumento}`;
+    
+    console.log(idAudio);
+    
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+        contador = (contador + 1);
+
+    console.log(contador);
+}
